1407dacc3f57ddd1b92f746df4c5b85b8fbf0bf5
Mr-Titos <Arthur.Titos2@gmail.com>
2019-09-30 01:30:42 +0200

    init repository

--- program.js
+++ program.js
@@ -2,17 +2,30 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require('fs');
 
-//Faire en sorte que idticket s'incrémente meme si il y a redémarrage (sauvegarde dans fichier text via FS)
 
-var prefix = "$";
+var prefix = "";
 var ticketchannel = 0;
+var channeladmin;
 let link = false;
+let linkadmin = false;
 var idticket = 0;
+var suprole = "";
+var save;
 var catticket = false;
 let categorie;
-
+let msgprblm = "";
 
 bot.on('ready', function () {
+    fs.readFile("save.txt",'utf-8', function(error, data) {
+        if (error) {
+            return console.log(err);
+          } else {
+          save = data.split(',');
+          idticket = save[0];
+          suprole = save[1];
+          prefix = save[2];
+         }
+    })
   console.log("Je suis connecté !");
 })
 
@@ -41,35 +54,45 @@ function C_Chan(resolve,serv,user) {
             allow: ['VIEW_CHANNEL'],
         },
         {
-            id: serv.roles.find(role => role.name === "Support").id,
+            id: serv.roles.find(role => role.name === suprole).id,
             allow: ['VIEW_CHANNEL'],
         },
 
     ]).then(channel => {
-        channel.setParent(resolve.id);
+        channel.setParent(resolve.id); // resolve est le channel categorie
+        channel.sendMessage("@everyone Le ticket est ouvert !");
+        if(msgprblm !== "") {
+            channel.sendMessage("Le probleme est le suivant : \r\n" + msgprblm);
+            msgprblm = "";
+        }
     }).catch(console.error);
     
     idticket++;
+    fs.writeFileSync("save.txt", idticket + ',' + save[1] + ',' + prefix, { encoding: 'utf8'});
+ 
+         
 }
 
 bot.on('message', msg => {
     if(msg.content.substring(0,1) === prefix && msg.author.id !== bot.user.id) {
         if(msg.content === prefix + 'help' ) {
             if(msg.member.hasPermission('ADMINISTRATOR')) {
-                msg.author.sendMessage("Prerequis: \nUn rôle nommé \"Support\" \nRéservé un channel spécifique pour la création de ticket(Je conseille de poster un message d'explication avant la réservation du channel) \n\nCommandes: \nPour appeler une commande il faut le préfixe plus le nom de la commande (+ arguments si nécessaire) \nlink: Permet de réserver un channel pour la création de tickets (Nécessite le droit Administrateur) \nticket: Permet a l'utilisateur de créer un ticket, doit être appelé dans le channel réservé par la commande link \nhelp: Permet d'envoyer ce texte en MP afin d'aider dans l'utilisation de ce bot (Nécessite le droit Administrateur) \n \nPour plus de renseignement contacter Titos#2671")
+                msg.author.sendMessage("Prerequis: \nUn rôle nommé \"Support\" \nRéserver un channel spécifique pour la création de ticket(Je conseille de poster un message d'explication avant la réservation du channel) \nRéserver un channel (de préférence seulement pour les admins) pour l'éxécution de commandes admin du bot \n\nCommandes: \nPour appeler une commande \"<prefix><commande> <argument>\" \nlink: Permet de réserver un channel pour la création de tickets (Nécessite le droit Administrateur) \nlinkadmin : Permet de réserver un channel pour éxécuter les commandes admins du bot (Nécessite le droit Administrateur) \nticket: Permet a l'utilisateur de créer un ticket, doit être appelé dans le channel réservé par la commande link \nhelp: Permet d'envoyer ce texte en MP afin d'aider dans l'utilisation de ce bot (Nécessite le droit Administrateur) \nprefix: Permet de changer le préfixe a utiliser avant d'appeler une commande (Nécessite le droit Administrateur)  \n \nPour plus de renseignement contacter Titos#2671")
                 .catch(function() { console.error(); });
             } else {
-                msg.author.sendMessage("Vous n'avez pas les droits nécéssaires pour consulter cette aide").catch(function() { console.error(); });
+                msg.author.sendMessage("Vous n'avez pas les droits nécessaires").catch(function() { console.error(); });
             }
             msg.delete(); 
         } 
             if(msg.channel.id === ticketchannel) {
-                    if(msg.content === prefix + 'ticket') {
+                    if(msg.content.substring(0,7) === prefix + 'ticket') {
+                            msgprblm = msg.content.substring(7);
                             var server = msg.guild;
                             var usertemp = msg.author;
                                     C_Cat(server).catch(function () {
                                         server.createChannel("Problemes/Tickets", "category").then(function() {
                                             msg.reply("La catégorie afin d'acceuillir les ticket a été crée, si vous voulez créer un ticket veuillez réitérer la commande ticket.");
+                                            msg.delete();
                                             catticket = true;
                                         })
                                     }).then(function(res) { 
@@ -80,15 +103,32 @@ bot.on('message', msg => {
                             msg.delete();
                     } 
                     
-                    // A faire apres, nécéssite une sauvegarde par server id, et une code de trame
-                    /*else if(msg.content.substring(0,7) === prefix + 'ticketrole ') {
-                        
-                    }*/
                     else {
                         msg.delete();
                     }
 
                 
+            }
+            else if(msg.channel.id === channeladmin ) {
+                if(msg.member.hasPermission('ADMINISTRATOR')) {
+                    //Pas vraiment utile cette commande et creer du traitement supplémentaire
+                    /*if(msg.content.substring(0,12) === prefix + 'ticketrole ') {
+                        fs.writeFileSync("save.txt", save[0] + ',' + msg.content.substring(12) +',' + prefix, { encoding: 'utf8'});
+                        msg.reply("Le role a bien été changé !")
+                    }*/
+                    if(msg.content.substring(0,8) === prefix + 'prefix ') {
+                        if(msg.content.substring(8).length === 1) {
+                            prefix = msg.content.substring(8);
+                            fs.writeFileSync("save.txt", save[0] + ',' + save[1] +',' + msg.content.substring(8), { encoding: 'utf8'});
+                            msg.reply("Le prefix a bien été changé ! N'oubliez pas de changez l'annonce ;)")
+                        } else {
+                            msg.reply("Veuillez indiquez un seul caractère pour le préfix")
+                        }
+                    }
+                }
+                else {
+                    msg.reply("Vous n'avez pas les droits nécessaires")
+                }
             }
             else {
                 if (msg.content === prefix +'link') {
@@ -104,27 +144,28 @@ bot.on('message', msg => {
                     }
                     
                 }
+                else if(msg.content === prefix +'linkadmin') {
+                    if(msg.member.hasPermission('ADMINISTRATOR')) {
+                        msg.reply('Link admin on ' + msg.channel.name);
+                        channeladmin = msg.channel.id;
+                        linkadmin = true;
+                    }
+                    else {
+                        msg.reply("Vous n'avez pas les droits nécessaires");
+                    }
+                }
                 else {
                     if(link === false && msg.content === prefix +'ticket') {
                         msg.reply("Veuillez d'abord me lier a un channel");
                     }
 
                 }
-                //Nécéssite ici aussi une sauvegarde via un fichier extérieur et tutti quanti
-                /*if (msg.content === prefix +'prefix') {
-                    if(msg.content.substring(8).length === 1) {
-                        prefix = msg.content.substring(8);
-                        msg.reply("Le prefix a bien été mis a jour");
-                    } else {
-                        msg.reply("Erreur lors de la commande : <ancien_préfix>prefix <nouveau_préfix>");
-                    }
-                }*/
             }
     }   
     if(msg.channel.id === ticketchannel && msg.content.substring(0,1) !== prefix) { 
         msg.delete();
     }
-  })
+  });
 
   
 bot.login('NTc5Mzk0NzA3MDcxODI3OTc4.XOBjpA.wHq1gZg0spPy4-F8GPJIUYc4BrQ');
